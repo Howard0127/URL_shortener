cdc1f14b0963c32a955edb5a0ae636e075462af6
Howard0127 <howard740127@gmail.ocm>
2021-09-28 22:48:01 +0800

    feat: add handlebars

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
+// require package used in the project
 const express = require('express')
 const mongoose = require('mongoose')
 const exphbs = require('express-handlebars')
+
 const app = express()
 
 mongoose.connect('mongodb://localhost/URL-shortener')
@@ -15,13 +17,16 @@ db.once('open', () => {
   console.log('mongodb connected!')
 })
 
+// setting template engine
 app.engine('hbs', exphbs({ defaultLayout: 'main', extname: '.hbs'}))
 app.set('view engine', 'hbs')
 
+// route setting
 app.get('/', (req, res) => {
   res.render('index')
 })
 
+// start and listen on the Express server
 app.listen(3000, () => {
   console.log('App is running on port http://localhost:3000.')
 })
