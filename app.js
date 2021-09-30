2e89b5ed49e79ad9eeb7e22296c50da0291a40fe
Howard0127 <howard740127@gmail.ocm>
2021-09-30 11:48:51 +0800

    refactor: modigy mongoose config

--- app.js
+++ app.js
@@ -1,23 +1,10 @@
 // require package used in the project
 const express = require('express')
-const mongoose = require('mongoose')
 const exphbs = require('express-handlebars')
 const bodyParser = require('body-parser')
-
 const routes = require('./routes')
+require('./config/mongoose')
 const app = express()
-
-mongoose.connect('mongodb://localhost/URL-shortener')
-
-const db = mongoose.connection
-
-db.on('error', () => {
-  console.log('mongodb error!')
-})
-
-db.once('open', () => {
-  console.log('mongodb connected!')
-})
 
 // setting template engine
 app.engine('hbs', exphbs({ defaultLayout: 'main', extname: '.hbs'}))
@@ -31,5 +18,5 @@ app.use(routes)
 
 // start and listen on the Express server
 app.listen(3000, () => {
-  console.log('App is running on port http://localhost:3000.')
+  console.log('App is running on http://localhost:3000.')
 })
